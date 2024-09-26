6c062b14df5b650115063dd780062d529962dcba
mrhacko <nettoag85@gmail.com>
2024-09-26 16:54:53 -0300

    Add files via upload

---
+++ ano_nascimento.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome?: ')
+ano_de_nascimento = input('em que ano você nasceu meu fi?: ')
+ano_atual = 2024
+Idade = ano_atual - int(ano_de_nascimento)
+print('Olá', nome, 'você nasceu no ano de ' , ano_de_nascimento, 'portanto você tem ' , Idade)
